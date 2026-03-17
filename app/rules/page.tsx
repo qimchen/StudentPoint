a13808ec2bc320c3497e60d556bf11368e1cbe8d
qimchen <qimchen.jimmy@gmail.com>
2026-03-17 09:47:24 +0800

    fix: 修复import路径错误

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,5 +1,5 @@
-import { getValue } from '../lib/kv';
-import type { Student, ScoreItem } from '../lib/types';
+import { getValue } from '../../lib/kv';
+import type { Student, ScoreItem } from '../../lib/types';
 import RulesClient from './components/RulesClient';
 
 export const dynamic = 'force-dynamic';
